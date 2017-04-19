6a703a249c8fb3470f0c8c6cd33a49829d9a9083
miles hill <miles1618@gmail.com>
2017-04-19 09:05:10 -0500

    Added build files

---
+++ dist/main.bundle.js
@@ -0,0 +1,382 @@
+webpackJsonp([1,5],{
+
+/***/ 329:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return SettingsComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var SettingsComponent = (function () {
+    function SettingsComponent() {
+    }
+    SettingsComponent.prototype.ngOnInit = function () {
+    };
+    SettingsComponent = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_6" /* Component */])({
+            selector: 'app-settings',
+            template: __webpack_require__(674),
+            styles: [__webpack_require__(671)]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], SettingsComponent);
+    return SettingsComponent;
+}());
+//# sourceMappingURL=/home/miles/Projects/Professional/AngularProjects/weatherApp/src/settings.component.js.map
+
+/***/ }),
+
+/***/ 330:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__weather_underground_service__ = __webpack_require__(331);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return WeatherComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var WeatherComponent = (function () {
+    function WeatherComponent(weatherService) {
+        this.weatherService = weatherService;
+    }
+    WeatherComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.getDefaultCity();
+        this.weatherService.getWeather(this.zmw)
+            .subscribe(function (weather) {
+            _this.weather = weather.current_observation;
+        });
+    };
+    // Service call to Weather API
+    // Accepts location object and queries 
+    // using the city zmw
+    WeatherComponent.prototype.getWeather = function (location) {
+        var _this = this;
+        this.weatherService.getWeather(location.zmw)
+            .subscribe(function (weather) {
+            _this.weather = weather;
+        });
+    };
+    // Called on `keyup` events in `input`
+    // Returns array of city objects matching input
+    WeatherComponent.prototype.getQuery = function () {
+        var _this = this;
+        this.weatherService.searchCities(this.searchStr)
+            .subscribe(function (res) {
+            _this.results = res.RESULTS;
+        });
+    };
+    // Initializes Houston, TX as default city
+    WeatherComponent.prototype.getDefaultCity = function () {
+        this.zmw = '77001.1.99999'; // Houston, Tx
+    };
+    // Invoked on `click` events after user city search.
+    // Result.zmw used to update location
+    WeatherComponent.prototype.setLocation = function (result) {
+        var _this = this;
+        this.zmw = result.zmw;
+        this.results = null;
+        this.weatherService.getWeather(this.zmw)
+            .subscribe(function (weather) {
+            _this.weather = weather.current_observation;
+        });
+    };
+    WeatherComponent = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_6" /* Component */])({
+            selector: 'app-weather',
+            template: __webpack_require__(675),
+            styles: [__webpack_require__(672)]
+        }), 
+        __metadata('design:paramtypes', [(typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__weather_underground_service__["a" /* WeatherUndergroundService */] !== 'undefined' && __WEBPACK_IMPORTED_MODULE_1__weather_underground_service__["a" /* WeatherUndergroundService */]) === 'function' && _a) || Object])
+    ], WeatherComponent);
+    return WeatherComponent;
+    var _a;
+}());
+//# sourceMappingURL=/home/miles/Projects/Professional/AngularProjects/weatherApp/src/weather.component.js.map
+
+/***/ }),
+
+/***/ 331:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_http__ = __webpack_require__(306);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs_add_operator_map__ = __webpack_require__(679);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs_add_operator_map___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_rxjs_add_operator_map__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return WeatherUndergroundService; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+var WeatherUndergroundService = (function () {
+    function WeatherUndergroundService(http) {
+        this.http = http;
+        this.apiKey = '50f6ae31331bce3b';
+        this.conditionsUrl = 'http://api.wunderground.com/api/' + this.apiKey + '/conditions/q';
+        this.searchUrl = 'http://localhost:4200/api/aq?query=';
+    }
+    WeatherUndergroundService.prototype.getWeather = function (zmw) {
+        return this.http.get(this.conditionsUrl + '/zmw:' + zmw + '.json')
+            .map(function (res) { return res.json(); });
+    };
+    WeatherUndergroundService.prototype.searchCities = function (searchStr) {
+        return this.http.get(this.searchUrl + searchStr)
+            .map(function (res) { return res.json(); });
+    };
+    WeatherUndergroundService = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["c" /* Injectable */])(), 
+        __metadata('design:paramtypes', [(typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__angular_http__["b" /* Http */] !== 'undefined' && __WEBPACK_IMPORTED_MODULE_1__angular_http__["b" /* Http */]) === 'function' && _a) || Object])
+    ], WeatherUndergroundService);
+    return WeatherUndergroundService;
+    var _a;
+}());
+//# sourceMappingURL=/home/miles/Projects/Professional/AngularProjects/weatherApp/src/weather-underground.service.js.map
+
+/***/ }),
+
+/***/ 389:
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = 389;
+
+
+/***/ }),
+
+/***/ 390:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__ = __webpack_require__(480);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__environments_environment__ = __webpack_require__(514);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__app_app_module__ = __webpack_require__(512);
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_2__environments_environment__["a" /* environment */].production) {
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["a" /* enableProdMode */])();
+}
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_3__app_app_module__["a" /* AppModule */]);
+//# sourceMappingURL=/home/miles/Projects/Professional/AngularProjects/weatherApp/src/main.js.map
+
+/***/ }),
+
+/***/ 511:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'app works!';
+    }
+    AppComponent = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_6" /* Component */])({
+            selector: 'app-root',
+            template: __webpack_require__(673),
+            styles: [__webpack_require__(670)]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+//# sourceMappingURL=/home/miles/Projects/Professional/AngularProjects/weatherApp/src/app.component.js.map
+
+/***/ }),
+
+/***/ 512:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__(148);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__(471);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__(306);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__(511);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__weather_underground_service__ = __webpack_require__(331);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__shared_weather_weather_component__ = __webpack_require__(330);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__shared_settings_settings_component__ = __webpack_require__(329);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__app_routes__ = __webpack_require__(513);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */],
+                __WEBPACK_IMPORTED_MODULE_6__shared_weather_weather_component__["a" /* WeatherComponent */],
+                __WEBPACK_IMPORTED_MODULE_7__shared_settings_settings_component__["a" /* SettingsComponent */]
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */],
+                __WEBPACK_IMPORTED_MODULE_3__angular_http__["a" /* HttpModule */],
+                __WEBPACK_IMPORTED_MODULE_8__app_routes__["a" /* routing */]
+            ],
+            providers: [__WEBPACK_IMPORTED_MODULE_5__weather_underground_service__["a" /* WeatherUndergroundService */]],
+            bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppModule);
+    return AppModule;
+}());
+//# sourceMappingURL=/home/miles/Projects/Professional/AngularProjects/weatherApp/src/app.module.js.map
+
+/***/ }),
+
+/***/ 513:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_router__ = __webpack_require__(500);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__shared_settings_settings_component__ = __webpack_require__(329);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__shared_weather_weather_component__ = __webpack_require__(330);
+/* unused harmony export routes */
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return routing; });
+
+
+
+// Route Configuration
+var routes = [
+    { path: 'settings', component: __WEBPACK_IMPORTED_MODULE_1__shared_settings_settings_component__["a" /* SettingsComponent */] },
+    { path: '', redirectTo: '/weather', pathMatch: 'full' },
+    { path: 'weather', component: __WEBPACK_IMPORTED_MODULE_2__shared_weather_weather_component__["a" /* WeatherComponent */] }
+];
+var routing = __WEBPACK_IMPORTED_MODULE_0__angular_router__["a" /* RouterModule */].forRoot(routes);
+//# sourceMappingURL=/home/miles/Projects/Professional/AngularProjects/weatherApp/src/app.routes.js.map
+
+/***/ }),
+
+/***/ 514:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `angular-cli.json`.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=/home/miles/Projects/Professional/AngularProjects/weatherApp/src/environment.js.map
+
+/***/ }),
+
+/***/ 670:
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ 671:
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ 672:
+/***/ (function(module, exports) {
+
+module.exports = "ul {\n  list-style-type: none;\n  padding: 0;\n  margin: 1em;\n}\n\n.main {\n    background: #333b5b;\n    color:#ffffff;\n}\n\n.temp {\n    font-size: 70px;\n    text-align: center;\n    font-weight: normal;\n}\n\n.location, .icon, .desc {\n    text-align: center;\n}\n\n#city-input{\n    margin-top: 0.5em;\n    padding-top: 1em;\n}\n"
+
+/***/ }),
+
+/***/ 673:
+/***/ (function(module, exports) {
+
+module.exports = "\n\n<ul class=\"menu\">\n  <li><a [routerLink]=\"['/weather']\">Weather</a></li>\n  <li><a [routerLink]=\"['/settings']\">Settings</a></li>\n</ul>\n\n  <router-outlet></router-outlet>\n"
+
+/***/ }),
+
+/***/ 674:
+/***/ (function(module, exports) {
+
+module.exports = "<p>\n  settings works!\n</p>\n"
+
+/***/ }),
+
+/***/ 675:
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"main\">\n\n  <div class=\"small-12 columns\" id=\"city-input\">\n    <input (keyup)=\"getQuery()\" [(ngModel)]=\"searchStr\" name=\"searchStr\" type=\"text\" value=\"\" placeholder=\"City\"/>\n    \n  </div>\n\n  <div *ngIf=\"results\">\n    <ul>\n      <li *ngFor=\"let result of results\" (click)=\"setLocation(result)\">{{result.name}}</li>\n    </ul>\n  </div>\n\n  <div class=\"row\" *ngIf=\"weather\">\n    <div class=\"small-12\">\n      <h2 class=\"location\">{{ weather.display_location.full }}</h2>\n      <div class=\"icon\"><img src=\"{{ weather.icon_url }}\"></div>\n      <h3 class=\"desc\">{{weather.weather}}</h3>\n      <h1 class=\"temp\">{{weather.temp_f}}&deg;</h1>\n    </div>\n\n    <div class=\"row\">\n      <div class=\"small-12 columns\">\n        <ul>\n          <li><strong>Temp: </strong>{{weather.temperature_string}}</li>\n          <hr>\n          <li><strong>Relative Humidity: </strong>{{weather.relative_humidity}}</li>\n          <hr>\n          <li><strong>Dewpoint: </strong>{{weather.dewpoint_string}}</li>\n          <hr>\n          <li><strong>Visibility: </strong>{{weather.visibility_mi}} miles</li>\n          <hr>\n          <li><strong>Wind: </strong>{{weather.wind_mph}} mph</li>\n          <hr>\n          <li><strong>Wind Direction: </strong>{{weather.wind_dir}}</li>\n          <hr>\n          <li><strong>Heat Index: </strong>{{weather.heat_index_string}}</li>\n        </ul>\n      </div>\n    </div>\n\n  </div>\n</div>"
+
+/***/ }),
+
+/***/ 694:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(390);
+
+
+/***/ })
+
+},[694]);
+//# sourceMappingURL=main.bundle.map
